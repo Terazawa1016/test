c7f70f02ee68205d64ea53ec933f8eb95e980202
Terazawa <soryo10to16@gmail.com>
2020-04-30 16:18:26 +0900

    no message

---
+++ app/Http/Controllers/Api/CartController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Cart;
+
+
+class CartController extends Controller
+{
+    public function delete($user_id)
+  {
+
+    Cart::where('user_id', $user_id)
+    ->delete();
+
+    return true;
+  }
+
+}
